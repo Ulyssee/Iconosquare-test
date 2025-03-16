39c71c6d5422f7c1d072516e17d1cda7761ad767
Ulyssee <ulysse.mf@hotmail.fr>
2025-03-16 16:16:56 +0100

    Add a button to pause/play.

--- src/utils/hooks/useLiveChartContext.tsx
+++ src/utils/hooks/useLiveChartContext.tsx
@@ -1,32 +1,52 @@
-import React, { useContext, useReducer, createContext, ReactNode, Dispatch } from 'react';
+import React, { createContext, useContext, useReducer, ReactNode } from 'react';
 import { createRandomEvent, EventType } from '../utils';
 
 interface LiveChartState {
   events: EventType[];
+  paused: boolean;
 }
+
+type LiveChartAction =
+  | { type: 'new_event'; payload: EventType }
+  | { type: 'toggle_pause' };
 
 interface LiveChartContextType {
   data: LiveChartState;
-  dispatch: Dispatch<LiveChartAction>;
+  dispatch: React.Dispatch<LiveChartAction>;
 }
 
-type LiveChartAction = { type: 'new_event'; payload: EventType };
+const initialEvents: EventType[] = Array.from({ length: 50 }, (_, ix) => createRandomEvent(ix));
+
+const initialData: LiveChartState = {
+  events: initialEvents,
+  paused: false, 
+};
 
 const LiveChartContext = createContext<LiveChartContextType | undefined>(undefined);
-
-const initialEvents: EventType[] = Array.from({ length: 50 }, (_, ix) => createRandomEvent(ix));
-const initialData: LiveChartState = { events: initialEvents };
 
 const liveChartReducer = (state: LiveChartState, action: LiveChartAction): LiveChartState => {
   switch (action.type) {
     case 'new_event':
-      return { events: [...state.events, action.payload] };
+      if (state.paused) {
+        return state;
+      }
+      return {
+        ...state,
+        events: [...state.events, action.payload],
+      };
+
+    case 'toggle_pause':
+      return {
+        ...state,
+        paused: !state.paused,
+      };
+
     default:
-      throw new Error(`Unhandled action type: ${action.type}`);
+      throw new Error(`Unhandled action type: ${action}`);
   }
 };
 
-export const LiveChartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+export const LiveChartProvider: React.FC<{ children?: ReactNode }> = ({ children }) => {
   const [data, dispatch] = useReducer(liveChartReducer, initialData);
 
   return (
@@ -35,10 +55,11 @@ export const LiveChartProvider: React.FC<{ children: ReactNode }> = ({ children 
     </LiveChartContext.Provider>
   );
 };
+
 export const useLiveChartContext = () => {
   const context = useContext(LiveChartContext);
   if (!context) {
-    throw new Error('useLiveChartContext must be used within a LiveChartProvider');
+    throw new Error('useLiveChartContext should be used within a LiveChartProvider');
   }
   return context;
 };
